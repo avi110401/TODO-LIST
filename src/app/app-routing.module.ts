3dcdbd6023acc271bc4b5ff18810f001838f94e2
avirup <avirup11042001@gmail.com>
2024-12-07 17:41:54 +0530

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { AboutComponent } from './MyComponents/about/about.component';
 import { TodosComponent } from './MyComponents/todos/todos.component';
 
 const routes: Routes = [
-  { path: '', component: TodosComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'TODO-LIST/', component: TodosComponent },
+  { path: 'TODO-LIST/about', component: AboutComponent },
 ];
 
 @NgModule({
